4f6c892ffad11ed7a3535fa46090df146dff30c3
Angela <a.c.lee@wustl.edu>
2025-11-12 14:10:23 -0600

    done

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -11,19 +11,27 @@ public class MultipleChoiceQuestion extends Question {
 	 * @param points
 	 * @param choices
 	 */
+	 
+	 private String[] choices;
+
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
 		// Call the super class constructor, then create and set
 		// instance variables for any values that aren't handled
 		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
 	
 	/**
 	 * Display the prompt for the question in addition to 
 	 * the choices present for the question.
 	 */
+	@Override
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		System.out.println(getPrompt());
+		for (int i = 0; i < choices.length; i++) {
+			System.out.println(choices[i]);
+		}
 	}
 	
 	/**
@@ -31,7 +39,7 @@ public class MultipleChoiceQuestion extends Question {
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return this.choices;
 	}
 	
 	public static void main(String[] args) {
--- src/studio8/Quiz.java
+++ src/studio8/Quiz.java
@@ -10,8 +10,9 @@ public class Quiz {
 	 * Constructor
 	 * @param questions
 	 */
+	private Question[] questions;
 	public Quiz(Question[] questions) {
-		throw new NotYetImplementedException();
+		this.questions = questions;
 	}
 	
 	/**
@@ -30,7 +31,11 @@ public class Quiz {
 	 * @return int number of total points
 	 */
 	public int getTotalPoints() {
-		throw new NotYetImplementedException();
+		int sum = 0;
+		for(int i = 0; i < questions.length; i++) {
+			sum = sum + questions[i].getPoints();
+		}
+		return sum;
 	}
 	
 	/**
@@ -41,7 +46,7 @@ public class Quiz {
 	 * @param in Scanner object to feed into getUserAnswer
 	 */
 	public void takeQuiz(Scanner in) {
-		throw new NotYetImplementedException();
+		System.out.println("You earned " + getTotalPoints() + " points");
 	}
 	
 	
